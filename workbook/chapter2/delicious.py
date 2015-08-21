87586f2949a2022ec6a388ed91c75883bef22923
Dylan Fox <dylan@crowdvance.com>
2015-08-20 22:19:05 -0700

    delicious mock stuff

--- workbook/chapter2/delicious.py
+++ workbook/chapter2/delicious.py
@@ -1,13 +1,6 @@
+import random
+
 class MockDeliciousAPI(object):
-
-	def __init__(self, sample_size=50):
-
-		def create_data(i):
-			href = 'http://www.foo.com/' + str(i)
-			user =  'user_' + str(i)
-			return {'href': href, 'user': user}
-
-		self.mock_popular_data = [create_data(i) for i in xrange(sample_size)]
 
 	def get_popular(self, tag):
 		"""
@@ -18,9 +11,43 @@ class MockDeliciousAPI(object):
 		* description (description)
 		* user (user)
 		"""
-		return self.mock_popular_data
+		def create_data(i):
+			href = 'http://www.foo.com/' + str(i)
+			user =  'user_' + str(i)
+			return {'href': href, 'user': user}
 
-	def get_urlposts(href):
+		mock_popular_data = [create_data(i) for i in xrange(50)]
+
+		return mock_popular_data
+
+	# def get_urlposts(href):
+	# 	"""
+	# 	Returns all the posts for a given URL. 
+	# 	Returns last 30 people to post the link.
+	# 	"""
+	# 	mock_urlposts = [{'user': 'user_'+ str(random.randrange(0, 100))} for i in xrange(30)]
+	# 	for i in xrange(30):
+	# 		data = {'user': 'user_' + str(random.randrange(0, 100))}
+	# 		if 
+	# 	return mock_urlposts
+
+
+	def get_userposts():
 		"""
-		Returns all the posts for a given URL
+		Returns all the posts for a given user
 		"""
+
+def initialize_user_dict(tag, count=5):
+	api = MockDeliciousAPI()
+	user_dict = {}
+
+	# get the top "count" popular posts
+	popular_posts = api.get_popular(tag=tag)[:count]
+	for popular_post in popular_posts:
+		# find all users who posted this post
+		post_url = popular_post['href']
+		for _post in get_urlposts(post_url):
+			user = _post['user']
+			user_dict[user] = {}
+
+	return user_dict
