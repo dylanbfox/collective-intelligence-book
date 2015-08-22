8acc96b84224947828311c78780a3f8a7bcc4477
Dylan Fox <dylanbfox@gmail.com>
2015-08-22 13:15:50 -0700

    Tanimoto score

--- workbook/chapter2/recommendations.py
+++ workbook/chapter2/recommendations.py
@@ -35,7 +35,50 @@ def sim_distance(ratings, person1, person2):
 	# Add up squares of all differences (euclidian distance score)
 	sum_of_squares = sum([pow(ratings[person1][rating] - ratings[person2][rating], 2) for rating in similar])
 
-	return 1/(1+sqrt(sum_of_squares))	
+	return 1/(1+sqrt(sum_of_squares))
+
+def sim_tanimoto_A(ratings, person1, person2):
+    '''
+    Return the Tanimoto score between two users. 
+
+	No validation is performed except same
+	length checks. It is assumed that
+	the caller passes properly weighted data.
+    '''
+
+    # Create N-dimensonal vectors.
+    # The two N-dimensional vectors (eg: critic or product)
+    # to find the Tanimoto coefficient for. MUST be
+    # of the same length    
+    vec1 = ratings[person1]
+    vec2 = ratings[person2]
+
+    for rating in vec1.copy():
+    	if rating not in vec2:
+    		vec1.pop(rating, None)
+
+    for rating in vec2.copy():
+    	if rating not in vec1:
+    		vec2.pop(rating, None)
+
+    N = len(vec1)
+
+    # make sure vectors are same length,
+    # ie ... make sure the number of features
+    # match. since we're comparing similarity, 
+    # we need two vectors w/ the same features to
+    # compare against each other
+    assert N == len(vec2)
+
+    v1v2, v1v1, v2v2 = 0., 0., 0.
+
+    # remember, N is number of features
+    for rating in vec1:
+        v1v2 += vec1[rating] * vec2[rating]
+        v1v1 += vec1[rating] * vec1[rating]
+        v2v2 += vec2[rating] * vec2[rating]
+
+    return v1v2 / (v1v1 + v2v2 - v1v2)
 
 # returns pearson correlation coefficient for person 1 and 2
 def sim_pearson(ratings, person1, person2):
@@ -95,6 +138,14 @@ def top_matches(ratings, person, n=5, algorithm=sim_pearson):
 
 
 def get_recommendations(ratings, person, algorithm=sim_pearson):
+	"""
+	User based filtering.
+
+	How similar is User A to User X, Y, and Z?
+	Use these similarity scores to figure out which
+	movies to recommend, based off what User X, Y, and Z
+	rated that User A hasn't seen yet.
+	"""
 	totals = {}
 	similarity_sums = {}
 
@@ -127,3 +178,59 @@ def get_recommendations(ratings, person, algorithm=sim_pearson):
 	rankings.sort()
 	rankings.reverse()
 	return rankings
+
+def get_recommended_items(ratings, similarity_set, user):
+	"""
+	Item based filtering.
+
+	Given how similar Product A is to B, C, and D, 
+	how likely is User A to purchase product B, C, or D.
+	Or, what rating is User A likely to give to product B, C, or D.
+	"""
+
+	user_ratings = rating[user]
+	scores = {}
+	total_sim = {}
+
+	# Loop over items rated by this user
+	for (item, rating) in user_ratings.items():
+
+		# loop over items similar to this one
+		for (similarity, item2) in similarity_set[item]:
+
+			# ignore if user already rated item
+			if item2 in user_ratings:
+				continue
+
+			# Weighted sum of rating for item2 * similarity score to item
+			# eg: 3.5 * 0.555 (if Phantom of Opera (item2) is 0.555 similar
+			# to Superman (item), and Superman (item) received a 3.5 rating)
+			scores.setdefault(item2, 0)
+			scores[item2] += similarity * rating
+
+			# sum of all the similarity scores of item2s compared to item
+			total_sim[item2] += similarity
+
+	# Divide each total score by total weight to get average
+	rankings = [(score / total_sim[item], item) for item, score in scores.items()]
+
+def load_movielens(path='data/movielens'):
+
+	# Get movie (product) titles
+	movies = {}
+	for line in open(path+'/u.item'):
+		(id, title) = line.split('|')[:2]
+		movies[id] = title
+
+	# Load data
+	ratings = {}
+	for line in open(path+'/u.data'):
+		(user, movieid, rating, ts) = line.split('\t')
+		ratings.setdefault(user, {})
+		movie_title = movies[movieid]
+		ratings[user][movie_title] = float(rating)
+
+	return ratings
+
+
+
