cccfb173b172575527f971e41a48d9f33084c272
Dylan Fox <dylanbfox@gmail.com>
2015-09-30 21:15:51 -0700

    K-Clusters algo

--- workbook/chapter3/clusters.py
+++ workbook/chapter3/clusters.py
@@ -182,3 +182,49 @@ def hcluster(rows, distance=pearson):
 		clust.append(newcluster)
 
 	return clust[0]
+
+def kcluster(rows, distance=pearson, k=4):
+	# determine the min and max values for each point
+	ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows])) 
+				for i in range(len(rows[0]))]
+
+	# create k randomly placed centroids
+	clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0] 
+				for i in range(len(rows[0]))] for j in range(k)]
+
+	lastmatches = None
+	for t in range(100):
+		print 'Iteration %d' % t
+		bestmatches = [[] for i in range(k)]
+
+		# find which centroid is the closest for each row
+		for j in range(len(rows)):
+			row = rows[j]
+			bestmatch = 0
+			for i in range(k):
+				d = distance(clusters[i], row)
+				if d < distance(clusters[bestmatch], row):
+					bestmatch = i
+			bestmatches[bestmatch].append(j)
+
+		# if results are same as last time, this is complete
+		if bestmatches == lastmatches:
+			break
+		lastmatches = bestmatches
+
+		# move the centroids to the average of their members
+		for i in range(k):
+			avgs = [0.0] * len(rows[0])
+			if len(bestmatches[i]) > 0:
+				for rowid in bestmatches[i]:
+					for m in range(len(rows[rowid])):
+						avgs[m] += rows[rowid][m]
+				for j in range(len(avgs)):
+					avgs[j] /= len(bestmatches[i])
+				
+				clusters[i] = avgs
+
+
+
+
+
