5f71742bc3fc5744b01930f609c03e8aa353aa58
Dylan Fox <dylanbfox@gmail.com>
2015-12-27 16:36:47 -0800

    init ch5

---
+++ workbook/chapter5/optimization.py
@@ -0,0 +1,12 @@
+import time
+import random
+import math
+
+people = [('Seymour', 'BOS'),
+		  ('Franny', 'DAL'),
+		  ('Zooey', 'CAK'),
+		  ('Walt', 'MIA'),
+		  ('Buddy', 'ORD'),
+		  ('Les', 'OMA')]
+
+destination = 'LGA'
